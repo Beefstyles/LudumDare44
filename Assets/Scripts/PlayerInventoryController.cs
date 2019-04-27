bb8c1b67b1e1b6b57f7ff6160520cabf7d82f1e6
Glen Willis <glenwillis@gmail.com>
2019-04-27 19:42:53 +0800

    Start of grid inventory.

--- Assets/Scripts/PlayerInventoryController.cs
+++ Assets/Scripts/PlayerInventoryController.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class PlayerInventoryController : MonoBehaviour
 {
-
-    void Start()
+    InventoryGridPosition[] inventoryGridPositions;
+    PlayerInventory playerInventory;
+    public int MaxX, MaxY;
+    
+    void Awake()
     {
-        
+        playerInventory = FindObjectOfType<PlayerInventory>();
+        inventoryGridPositions = GetComponentsInParent<InventoryGridPosition>();
+        for (int x = 0; x <= MaxX; x++)
+        {
+            for (int y = 0; y <= MaxY; y++)
+            {
+                foreach (var gridPos in inventoryGridPositions)
+                {
+                    string loc = string.Format("{0},{1}", x, y);
+                    if (gridPos.InventoryGridPositionLoc == loc)
+                    {
+                        gridPos.GridPositionIsAvailable = true;
+                    }
+
+                }
+            }
+        }
     }
 
     void Update()
