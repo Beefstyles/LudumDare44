8632fdaaf29f22ac21cf9027ef7902b9e4dfe7da
Glen Willis <glenwillis@gmail.com>
2019-04-27 10:37:03 +0800

    Have an idea now. Hopefully I have time to implement it.

---
+++ Assets/Scripts/CrosshairChange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairChange : MonoBehaviour
+{
+    public Texture2D DefaultTexture;
+    public Texture2D PickUpTexture;
+    public Texture2D DigTexture;
+    public CursorMode curMode;
+    public Vector2 HotSpot = Vector2.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseEnter()
+    {
+        if(gameObject.tag == "DigSpot")
+        {
+            Cursor.SetCursor(DigTexture, HotSpot, curMode);
+        }
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(DefaultTexture, HotSpot, curMode);
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField]
+    private int playerSpeed;
+
+    public int PlayerSpeed { get => playerSpeed; set => playerSpeed = value; }
+
+    void Update()
+    {
+        transform.position += Vector3.right * Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime;
+        transform.position += Vector3.up * Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        transform.LookAt(ray.GetPoint(0), Vector3.forward);
+
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+        
+    }
+}
